ba3a6c9c700b7160eb4ef6facdedd7bedfeb7508
Lenz <Lenz.Furrer@gmail.com>
2020-06-04 07:00:21 +0200

    :pushpin: move to poetry for dependency management

--- bconv/__init__.py
+++ bconv/__init__.py
@@ -5,7 +5,7 @@ bconv: converter for bio-NLP formats.
 
 __author__ = "Lenz Furrer"
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 
 from .fmt import load, loads, fetch, dump, dumps, LOADERS, FETCHERS, EXPORTERS
