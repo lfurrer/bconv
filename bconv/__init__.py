e57183d81c48e73cec771fc5000eae25169b8f13
Lenz <Lenz.Furrer@gmail.com>
2020-12-07 08:24:34 +0100

    :bookmark: bump version (0.6.0)

--- bconv/__init__.py
+++ bconv/__init__.py
@@ -5,7 +5,7 @@ bconv: converter for bio-NLP formats.
 
 __author__ = "Lenz Furrer"
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 
 from .fmt import load, loads, fetch, dump, dumps, LOADERS, FETCHERS, EXPORTERS
